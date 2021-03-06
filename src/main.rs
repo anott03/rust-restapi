21502b850a0b8c6949a99a8b58643a107a723405
Amitav Nott <amitavnott@parakeet.local>
2021-03-05 23:07:58 -0500

    "working on api"

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ use warp::{Filter, http};
 use parking_lot::RwLock;
 use std::collections::HashMap;
 use std::sync::Arc;
+use serde::{Serialize, Deserialize};
 
-type items = HashMap<String, i32>;
+type Items = HashMap<String, i32>;
 
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct Item {
     name: String,
     quantity: i32
@@ -24,16 +25,31 @@ impl Store {
     }
 }
 
-async fn add_list_item(item: Item, store: Store) -> Result<impl warp::Reply, warp:: Rejection> {
+fn json_body() -> impl Filter<Extract = (Item,), Error = warp::Rejection> + Clone {
+    warp::body::content_length_limit(1024 * 16).and(warp::body::json())
+}
+
+async fn add_list_item(item: Item, store: Store) -> Result<impl warp::Reply, warp::Rejection> {
     store.grocery_list.write().insert(item.name, item.quantity);
-    Ok(warp::reply::with_status(
+    println!("{:?}", store.grocery_list);
+    return Ok(warp::reply::with_status(
             "Added items to list",
             http::StatusCode::CREATED,
-    ))
+    ));
 }
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    let store = Store::new();
+    let store_filter = warp::any().map(move || store.clone());
+
+    let add_items = warp::post()
+        .and(warp::path("v1"))
+        .and(warp::path("groceries"))
+        .and(warp::path::end())
+        .and(json_body())
+        .and(store_filter.clone())
+        .and_then(add_list_item);
+
+    warp::serve(add_items).run(([127, 0, 0, 1], 3030)).await;
 }
