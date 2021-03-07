8fb86753f063933275a508fc219ce7757036cb98
anott03 <amitav_nott@ryecountryday.org>
2021-03-06 20:14:40 -0500

    stuff

--- src/database.rs
+++ src/database.rs
@@ -1,3 +1,15 @@
 pub fn test_database() {
     println!("database!");
 }
+
+pub fn connect_to_database() {
+    println!("connecting to database");
+}
+
+pub fn add_item() {
+    println!("adding item to database")
+}
+
+pub fn delete_item() {
+    println!("deleting item from database");
+}
